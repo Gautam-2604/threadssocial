ac2764b1cd820b38277d2a9055ef6c432d5d71ad
Gautam-2604 <gaurisariag@gmail.com>
2024-05-15 16:07:40 +0530

    login user route created

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import User from "../models/userModel.js";
 import brcypt from 'bcryptjs'
+import generateTokenAndSetCookie from "../utils/helpers/generateTokenAndSetCookie.js";
 
 const signupUser = async(req,res)=>{
     try {
@@ -23,6 +24,7 @@ const signupUser = async(req,res)=>{
         await newUser.save()
 
         if(newUser){
+            generateTokenAndSetCookie(newUser._id,res)
             res.status(201).json({
                  _id:newUser._id,
                 name:newUser.name,
@@ -38,5 +40,30 @@ const signupUser = async(req,res)=>{
     }
 }
 
+const loginUser = async(req,res)=>{
+    try {
+        const {username,password}=req.body;
+        const user = await User.findOne({username});
+        if(!user){
+            res.status(400).json({message:"Username doest not exists"})
+        }
+        const isPasswordCorrect = await brcypt.compare(password,user?.password)
+        if(!isPasswordCorrect){
+            res.status(400).json({message:"Password incorrect"})
+        }
 
-export {signupUser}
+        generateTokenAndSetCookie(user._id,res)
+        res.status(200).json({
+            _id:user._id,
+            name:user.name,
+            email:user.email,
+            username:user.username
+        })
+    } catch (error) {
+        res.status(500).json({message:"Error is Login User"})
+        console.log(error.message);
+    }
+}
+
+
+export {signupUser, loginUser}
