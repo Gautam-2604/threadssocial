6e31a102e3685ea0e65e0b39d6b0110c5cb32f4c
Gautam-2604 <gaurisariag@gmail.com>
2024-05-16 12:04:20 +0530

    Get feed and Reply post controllers added

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -71,6 +71,7 @@ const likeUnlikePost = async (req,res)=>{
             res.status(200).json({message:"Post unliked successsfully"})
         }else{
             //like Post
+            
             await Post.updateOne({_id:postId},{$push:{likes:userId}}) 
             res.status(200).json({message:"Post liked successsfully"})
         }
@@ -78,4 +79,47 @@ const likeUnlikePost = async (req,res)=>{
         res.status(500).json({message:error.message})
     }
 }
-export { createPost, getPost, deletePost, likeUnlikePost}
+
+const replyPost = async(req,res)=>{
+    try {
+        const {text} = req.body
+        const {id:postId} = req.params
+        const userId = req.user._id
+        const userProfilePic = req.user.profilePic
+        const username = req.user.username
+
+        if(!text){
+            res.status(400).json({message:"No reply"})
+        }
+
+        const post = await Post.findById(postId)
+        if(!post){
+            res.status(404).json({message:"Post not found"})
+        }
+
+        const reply = {userId, userProfilePic,username, text}
+        post.replies.push(reply)
+        await post.save()
+        res.status(200).json({message:"Reply added successfully", post})
+
+    } catch (error) {
+        res.status(500).json({message:error.message})
+    }
+}
+
+const getFeed = async(req,res)=>{
+    try {
+        const userId = req.user._id;
+        const user  = await User.findById(userId)
+        if(!user){
+            return res.status(404).json({message:"User not found"})
+        }
+        const following = user.following
+        const feedPosts = await Post.find({postedBy:{$in:following}}).sort({createdAt : -1})
+        res.status(200).json({feedPosts})
+    } catch (error) {
+        res.status(500).json({message:error.message})
+    }
+}
+
+export { createPost, getPost, deletePost, likeUnlikePost, replyPost, getFeed}
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,10 +1,12 @@
 import protectRoute from '../middlewares/protectRoute.js'
 import express from 'express'
-import { createPost, deletePost, getPost, likeUnlikePost } from '../controllers/postController.js';
+import { createPost, deletePost, getFeed, getPost, likeUnlikePost, replyPost } from '../controllers/postController.js';
 
 const router = express.Router()
 router.get('/:id', getPost)
+router.get('/feed', protectRoute ,getFeed)
 router.post('/create', protectRoute ,createPost);
 router.delete('/:id', protectRoute ,deletePost)
 router.post("/like/:id", protectRoute, likeUnlikePost)
+router.post("/reply/:id", protectRoute, replyPost)
 export default router
