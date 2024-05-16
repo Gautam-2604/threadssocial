bafc332188ad1e45403579c6082ef288e5c18dd5
Gautam-2604 <gaurisariag@gmail.com>
2024-05-16 17:05:51 +0530

    login Card connected

--- frontend/src/components/LoginCard.jsx
+++ frontend/src/components/LoginCard.jsx
@@ -18,10 +18,42 @@ import {
   import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
 import { useSetRecoilState } from 'recoil';
 import authScreenAtom from '../atoms/authAtom';
+import useShowToast from "../hooks/useShowToast";
+import userAtom from "../atoms/userAtom";
   
   export default function LoginCard() {
     const [showPassword, setShowPassword] = useState(false);
     const setAuthScreen = useSetRecoilState(authScreenAtom);
+    const setUser = useSetRecoilState(userAtom);
+	const [loading, setLoading] = useState(false);
+  const [inputs, setInputs] = useState({
+		username: "",
+		password: "",
+	});
+  const showToast = useShowToast();
+    const handleLogin = async ()=>{
+      setLoading(true);
+      try {
+        const res = await fetch("/api/users/login",{
+          method:"POST",
+          headers:{
+            "Content-Type":"application/json"
+          },
+          body:JSON.stringify(inputs)
+        });
+        const data = await res.json();
+			if (data.error) {
+				showToast("Error", data.error, "error");
+				return;
+			}
+			localStorage.setItem("user-threads", JSON.stringify(data));
+			setUser(data);
+      } catch (error) {
+        showToast("Error",error,"error")
+      }finally{
+        setLoading(false)
+      }
+    }
     return (
       <Flex
         
@@ -75,7 +107,9 @@ import authScreenAtom from '../atoms/authAtom';
                   color={'white'}
                   _hover={{
                     bg: useColorModeValue("gray.700","gray.800"),
-                  }}>
+                  }}
+                  onClick={handleLogin}
+                  >
                   Login
                 </Button>
               </Stack>
