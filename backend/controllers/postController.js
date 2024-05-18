a317c5e1f1b12616fb68305f1a0ba691351c86d9
Gautam-2604 <gaurisariag@gmail.com>
2024-05-18 17:05:23 +0530

    Feed added

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -116,14 +116,14 @@ const replyPost = async(req,res)=>{
 
 const getFeed = async(req,res)=>{
     try {
-        const userId = req.user._id;
+        const userId = req.user?._id;
         const user  = await User.findById(userId)
         if(!user){
             return res.status(404).json({message:"User not found"})
         }
         const following = user.following
         const feedPosts = await Post.find({postedBy:{$in:following}}).sort({createdAt : -1})
-        res.status(200).json({feedPosts})
+        res.status(200).json(feedPosts)
     } catch (error) {
         res.status(500).json({message:error.message})
     }
--- frontend/src/pages/UserPage.jsx
+++ frontend/src/pages/UserPage.jsx
@@ -3,12 +3,14 @@ import UserPost from "../components/UserPost"
 import { useState, useEffect } from "react"
 import {useParams} from 'react-router-dom'
 import useShowToast from '../hooks/useShowToast.js'
+import { Flex, Spinner } from "@chakra-ui/react"
 
 const UserPage = () => {
 
   const [user, setUser] = useState(null)
   const {username} = useParams()
   const showToast = useShowToast()
+  const [loading, setLoading]=useState(true)
 
   useEffect(() => {
     const getUser = async()=>{
@@ -25,14 +27,23 @@ const UserPage = () => {
         };
       } catch (error) {
        showToast('Error',error,'error')
+      }finally{
+        setLoading(false)
       }
     };
     getUser()
   }
   , [username, showToast])
   
-  if(!user)
-  return null;
+  if (!user && loading) {
+		return (
+			<Flex justifyContent={"center"}>
+				<Spinner size={"xl"} />
+			</Flex>
+		);
+	}
+
+	if (!user && !loading) return <h1>User not found</h1>;
   return (
     <>
     <UserHeader user={user} />
